f1d5e134d9f33d41670503d8235f5fe6f196352c
Arbias <k.arbias@live.com>
2019-11-03 00:00:13 +0100

    Fixed square colors and generated a random number

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ var colors = [
 	"rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,9 +19,22 @@ for(var i = 0; i < squares.length; i++){
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 			if(clickedColor === pickedColor){
-				alert("Correct");
-			}else{
-				alert("Wrong");
+				messageDisplay.textContent = "Correct!";
+				changeColors(clickedColor);
+			}else { 
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
 			}
 	});
+}
+
+function changeColors(color){
+	for(var i = 0; i < colors.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
