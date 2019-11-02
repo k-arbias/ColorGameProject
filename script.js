bfbcd07b65381e29a148deee83c2ab855d5218cc
Arbias <k.arbias@live.com>
2019-11-03 00:35:35 +0100

    Added a button for generating new colors to the game

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+	 colors = generateRandomColors(6);
+	 pickedColor = pickColor();
+	 colorDisplay.textContent = pickedColor;
+	 for(var i = 0; i < squares.length; i++){
+	 	squares[i].style.backgroundColor = colors[i];
+	 }
+	 h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -14,6 +25,7 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 			if(clickedColor === pickedColor){
 				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play Again ?";
 				changeColors(clickedColor);
 				h1.style.backgroundColor = clickedColor;
 			}else { 
