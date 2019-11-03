da16dc46f39d2d4561eb59492de58be45f4a4a5a
Arbias <k.arbias@live.com>
2019-11-03 11:00:02 +0100

    Added style to squares fonts and buttons

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ resetButton.addEventListener("click", function(){
 	 for(var i = 0; i < squares.length; i++){
 	 	squares[i].style.backgroundColor = colors[i];
 	 }
-	 h1.style.backgroundColor = "#232323";
+	 h1.style.backgroundColor = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
